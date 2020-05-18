c414c4bd01c98ba47ae05b4cb088663bbee5147e
chatflip <oo.chat.flip@gmail.com>
2020-05-19 01:12:16 +0900

    add execution script

---
+++ inceptionv3_res224.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+output_dir=inceptionv3
+export OMP_NUM_THREADS=16
+#python -m torch.distributed.launch --nproc_per_node=2 --use_env train.py \
+#       --model inceptionv3_res224 --batch-size 256 --epochs 100 \
+#       --lr 0.0045 --wd 0.00004 --lr-step-size 1 --lr-gamma 0.94 --cache-dataset \
+#       --output-dir=${output_dir} --apex
+python -m torch.distributed.launch --nproc_per_node=2 --use_env train.py \
+       --model inceptionv3_res224 --batch-size 256 --epochs 100 \
+       --lr 0.0045 --wd 0.00004 --lr-step-size 1 --lr-gamma 0.94 --cache-dataset \
+       --resume ${output_dir}/checkpoint.pth --output-dir=${output_dir} --apex
+#python -m torch.distributed.launch --nproc_per_node=2 --use_env train.py \
+#       --model inceptionv3_res224 --batch-size 256 --epochs 100 \
+#       --lr 0.0045 --wd 0.00004 --lr-step-size 1 --lr-gamma 0.94 --cache-dataset \
+#       --resume ${output_dir}/checkpoint.pth --output-dir=${output_dir} --apex --test-only
---
+++ mobilenetv2_width_mult13.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+output_dir=mobilenetv2
+export OMP_NUM_THREADS=16
+#python -m torch.distributed.launch --nproc_per_node=2 --use_env train.py \
+#       --model mobilenetv2_width_multi13 --batch-size 128 --epochs 300 \
+#       --lr 0.045 --wd 0.00004 --lr-step-size 1 --lr-gamma 0.98 --cache-dataset \
+#       --output-dir=${output_dir} --apex
+python -m torch.distributed.launch --nproc_per_node=2 --use_env train.py \
+       --model mobilenetv2_width_multi13 --batch-size 128 --epochs 300 \
+       --lr 0.045 --wd 0.00004 --lr-step-size 1 --lr-gamma 0.98 --cache-dataset \
+       --resume ${output_dir}/checkpoint.pth --output-dir=${output_dir} --apex
+#python -m torch.distributed.launch --nproc_per_node=2 --use_env train.py \
+#       --model mobilenetv2_width_multi13 --batch-size 128 --epochs 300 \
+#       --lr 0.045 --wd 0.00004 --lr-step-size 1 --lr-gamma 0.98 --cache-dataset \
+#       --resume ${output_dir}/checkpoint.pth --output-dir=${output_dir} --apex --test-only
